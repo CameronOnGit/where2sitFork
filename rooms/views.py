04905defba39c99d94201d7c7dda8a888a96948e
CameronOnGit <Cameron.jardine31@gmail.com>
2026-03-21 15:59:45 -0400

    Update views.py

--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from .models import Room
 
+
 # Create your views here.
 
+
+#def room_list(request):
+#   rooms = Room.objects.all()
+ #  return render(request, "rooms/room_list.html", {"rooms": rooms})
+
+
+
+
 def room_list(request):
-    rooms = Room.objects.all()
+   rooms = Room.objects.all()
+   min_capacity = request.GET.get("min_capacity")
+
+
+   if min_capacity:
+       try:
+           minimum = int(min_capacity)
+           rooms = rooms.filter(capacity__gte = minimum)
+       except ValueError:
+           pass
+           
     return render(request, "rooms/room_list.html", {"rooms": rooms})
