a17d4ba851db77db33c3edb44856a4eb4a6f1bbf
XYZ <rammasunwinner@gmail.com>
2026-03-29 17:01:30 -0400

    More rooms in seed.py and update gitignore

--- rooms/management/commands/seed.py
+++ rooms/management/commands/seed.py
@@ -1,11 +1,13 @@
 from django.core.management.base import BaseCommand
 from rooms.models import Building, Room
 
-def create_room(building, number, capacity):
-    Room.objects.create(building=building, number=number, capacity=capacity)
 
 class Command(BaseCommand):
     help = "Seed the database with sample data"
+
+    @staticmethod
+    def create_room(building, number, capacity):
+        Room.objects.create(building=building, number=number, capacity=capacity)
 
     def handle(self, *args, **kwargs):
         Building.objects.all().delete()
@@ -14,26 +16,36 @@ class Command(BaseCommand):
         b1 = Building.objects.create(name="NAC")
         b2 = Building.objects.create(name="Shepard Hall")
         b3 = Building.objects.create(name="Marshak")
+        b4 = Building.objects.create(name="Steinman")
 
         # NAC
-        create_room(b1, "0/201", 60)
-        create_room(b1, "4/113", 40)
-        create_room(b1, "6/111", 40)
-        create_room(b1, "6/112", 30)
-        create_room(b1, "6/113", 40)
-        create_room(b1, "6/310", 30)
-        create_room(b1, "7/313A", 30)
+        self.create_room(b1, "1/203", 90)
+        self.create_room(b1, "1/511E", 35)
+        self.create_room(b1, "4/113", 40)
+        self.create_room(b1, "6/111", 40)
+        self.create_room(b1, "6/112", 30)
+        self.create_room(b1, "6/113", 40)
+        self.create_room(b1, "6/121", 30)
+        self.create_room(b1, "6/310", 30)
+        self.create_room(b1, "7/313A", 30)
 
         # SH
-        create_room(b2, "201", 30)
-        create_room(b2, "204", 30)
+        self.create_room(b2, "201", 30)
+        self.create_room(b2, "210", 45)
+        self.create_room(b2, "203", 30)
+        self.create_room(b2, "204", 30)
+        self.create_room(b2, "378", 30)
 
         # Marshak
-        create_room(b3, "1307", 30)
-        create_room(b3, "1026", 30)
-        create_room(b3, "1123", 30)
-        create_room(b3, "MR1", 100)
-        create_room(b3, "MR2", 240)
-        create_room(b3, "MR3", 240)
+        self.create_room(b3, "410", 30)
+        self.create_room(b3, "1307", 30)
+        self.create_room(b3, "1026", 30)
+        self.create_room(b3, "1123", 30)
+        self.create_room(b3, "MR1", 100)
+        self.create_room(b3, "MR2", 240)
+        self.create_room(b3, "MR3", 240)
+
+        # Steinman
+        self.create_room(b4, "161", 100)
 
         self.stdout.write(self.style.SUCCESS("Database seeded successfully!"))
