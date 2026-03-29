2df4d11ce0afa26b4b51231d4a734ae44369ab30
Bai-Mouse <yixuanshi059@gmail.com>
2026-03-29 19:46:32 -0400

    add the reservation page

--- rooms/views.py
+++ rooms/views.py
@@ -31,4 +31,35 @@ def room_list(request):
         except ValueError:
             pass
 
-    return render(request, "rooms/room_list.html", {"rooms": rooms})
+    return render(request, "rooms/room_list.html", context)
+
+
+# Reservation view (no login required)
+from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
+from django.shortcuts import redirect
+
+@csrf_exempt
+def reservation(request):
+    from .models import Room
+    rooms = Room.objects.select_related('building').all()
+    success = False
+    error = None
+    if request.method == 'POST':
+        room_id = request.POST.get('room')
+        date = request.POST.get('date')
+        time_ = request.POST.get('time')
+        duration = request.POST.get('duration')
+        name = request.POST.get('name')
+        if not (room_id and date and time_ and duration):
+            error = 'Please fill in all required fields.'
+        else:
+            # Here you would normally save the reservation to the database
+            # For now, just show success (no model yet)
+            success = True
+    context = {
+        'rooms': rooms,
+        'success': success,
+        'error': error,
+    }
+    return render(request, "rooms/reservation.html", context)
